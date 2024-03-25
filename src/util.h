b22d4694cacdd7ce1a27492f581009b1adeef963
bitterbloom <bittterblooom+github@gmail.com>
2024-03-25 18:24:13 +0100

    Minor change

--- src/util.h
+++ src/util.h
@@ -1,10 +1,10 @@
 #pragma once
 
+#include <stdio.h>
 #ifdef DEBUG
 #define DEBUG_PRINT(fmt, ...) (printf(fmt, __VA_ARGS__))
 #else
-inline void ____ignore____(...) {}
-#define DEBUG_PRINT(fmt, ...) (____ignore____(__VA_ARGS__))
+#define DEBUG_PRINT(fmt, ...)
 #endif
 
 #define EXIT_PRINT(fmt, ...) (fprintf(stderr, "Error: \033[1;31m" fmt "\033[0m\n" __VA_OPT__(,) __VA_ARGS__), exit(EXIT_FAILURE))
